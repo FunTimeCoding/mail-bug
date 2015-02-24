8918a3b6fff78469a999c03d496bafd569ac26e1
Alexander Reitzel <funtimecoding@gmail.com>
2015-02-24 11:26:21 +0100

    extract yaml config reading and writing into a separate module

--- lib/mail_bug.py
+++ lib/mail_bug.py
@@ -1,66 +1,29 @@
 import getpass
 import imaplib
-import yaml
 import email
 import subprocess
-from os.path import expanduser
+import keyring
 from email.parser import BytesHeaderParser
-
-try:
-    import keyring
-
-    keyring_available = True
-except ImportError:
-    print('Password will not be saved because keyring is not available.')
-    keyring_available = False
+from lib.yaml_config import YamlConfig
 
 
 class MailBug:
-    configPath = ''
-
     def __init__(self):
-        home = expanduser('~')
-        self.configPath = home + '/.mail-bug.conf'
-
-    def get_username(self):
-        result = ''
-        try:
-            stream = open(self.configPath, 'r')
-            config_tree = yaml.load(stream)
-            result = config_tree['username']
-            stream.close()
-        except IOError:
-            print('Config ' + self.configPath + ' not found.')
-
-        return result
-
-    def save_username(self, name):
-        try:
-            stream = open(self.configPath, 'w')
-            stream.write('')
-            data = {'username': name}
-            stream.write(yaml.dump(data, default_flow_style=False))
-            stream.close()
-        except IOError as e:
-            print('Error while saving ' + self.configPath + ': ' + e.strerror)
+        self.config = YamlConfig('~/.mail-bug.conf')
 
     @staticmethod
     def get_password_for_username(username):
-        result = ''
-        if keyring_available:
-            password = keyring.get_password('mail-bug', username)
-            if password is not None:
-                result = password
-            else:
-                print('Password not yet stored in keyring.')
-
-        return result
+        password = keyring.get_password('mail-bug', username)
+        if password is not None:
+            return password
+        else:
+            print('Password not yet stored in keyring.')
+            return ''
 
     @staticmethod
     def save_password_for_username(username, password):
-        if keyring_available:
-            print('Storing password in keyring.')
-            password = keyring.set_password('mail-bug', username, password)
+        print('Storing password in keyring.')
+        keyring.set_password('mail-bug', username, password)
 
     @staticmethod
     def get_mail_text(message):
@@ -77,35 +40,31 @@ class MailBug:
         print('Text: ' + text)
         cmd = '/usr/local/bin/growlnotify -n mail-bug -m ' + text
         stat = subprocess.call(cmd, shell=True, executable='/bin/zsh')
-        print('Stat: ' + stat)
+        print('Stat: %d ' % stat)
 
     def run(self):
-        name = self.get_username()
+        name = self.config.get('username')
         if name == '':
             name = input('Username: ')
-            self.save_username(name)
+            self.config.set('username', name)
+            self.config.save()
 
-        passwordWasLoaded = False
+        password_loaded = False
         password = self.get_password_for_username(name)
         if password == '':
             password = getpass.getpass()
         else:
-            passwordWasLoaded = True
+            password_loaded = True
 
         try:
             mail = imaplib.IMAP4_SSL('imap.gmail.com')
             mail.login(name, password)
-            if not passwordWasLoaded:
+            if not password_loaded:
                 self.save_password_for_username(name, password)
-        except Exception as e:
-            print('Error: ' + e.message)
 
-        try:
             mail.select()
-
             result, data = mail.uid('search', None, 'ALL')
             elements = data[0].split()
-
             header_parser = BytesHeaderParser()
             # for element in elements:
             # result, data = mail.element('fetch', element, '(BODY[HEADER])')
@@ -128,10 +87,10 @@ class MailBug:
                     mail.uid('store', element, '+FLAGS', '\\Deleted')
 
             mail.expunge()
-
             mail.close()
             mail.logout()
+
         except Exception as e:
-            print('Error: ' + e.message)
+            print('Error: ' + format(e))
 
         return 0
