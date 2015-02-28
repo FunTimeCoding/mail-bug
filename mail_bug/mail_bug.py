79d5c3786afe2afb0bee5162b774f7fbb43b9e2a
Alexander Reitzel <funtimecoding@gmail.com>
2015-02-28 18:56:21 +0100

    address pylint warnings, improve argparse

--- mail_bug/mail_bug.py
+++ mail_bug/mail_bug.py
@@ -1,3 +1,4 @@
+import argparse
 import getpass
 import imaplib
 import email
@@ -8,32 +9,56 @@ from python_utility.yaml_config import YamlConfig
 
 
 class MailBug:
-    def __init__(self):
-        self.config = YamlConfig('~/.mail-bug.conf')
+    def __init__(self, arguments: list):
+        args = self.parse_args(arguments)
+        self.verbose = args.verbose
+        self.command = args.command
+        self.config = YamlConfig(args.config_file)
 
     @staticmethod
-    def get_password_for_username(username):
-        password = keyring.get_password('mail-bug', username)
-        if password is not None:
-            return password
-        else:
-            print('Password not yet stored in keyring.')
-            return ''
+    def parse_args(arguments: list=None):
+        description = 'Process mails automatically.'
+        parser = argparse.ArgumentParser(description=description)
 
-    @staticmethod
-    def save_password_for_username(username, password):
-        print('Storing password in keyring.')
-        keyring.set_password('mail-bug', username, password)
+        parser.add_argument(
+            '-v',
+            '--verbose',
+            help='Turn on some verbose messages.',
+            action='store_true'
+        )
+
+        parser.add_argument(
+            '-c',
+            '--config_file',
+            help='Config file.',
+            default='~/.mail-bug.conf'
+        )
+
+        valid_commands = MailBug.get_valid_commands()
+        parser.add_argument(
+            'command',
+            metavar='COMMAND',
+            nargs='?',
+            type=str,
+            help='Command to run.',
+            default=valid_commands[0],
+            choices=valid_commands
+        )
+
+        return parser.parse_args(arguments)
 
     @staticmethod
     def get_mail_text(message):
         maintype = message.get_content_maintype()
+        result = ''
         if maintype == 'multipart':
             for part in message.get_payload():
                 if part.get_content_maintype() == 'text':
-                    return part.get_payload()
+                    result = part.get_payload()
+                    break
         elif maintype == 'text':
-            return message.get_payload()
+            result = message.get_payload()
+        return result
 
     @staticmethod
     def parse_text(text):
@@ -43,54 +68,67 @@ class MailBug:
         print('Stat: %d ' % stat)
 
     def run(self):
-        name = self.config.get('username')
-        if name == '':
-            name = input('Username: ')
-            self.config.set('username', name)
+        try:
+            if self.command == 'eat':
+                result = self.eat_command()
+            else:
+                result = 0
+        except KeyboardInterrupt:
+            result = 1
+            print('\nUser aborted.')
+
+        return result
+
+    def eat_command(self):
+        username = self.config.get('username')
+        if username == '':
+            username = input('Username: ')
+            self.config.set('username', username)
             self.config.save()
 
+        app_name = 'mail-bug'
         password_loaded = False
-        password = self.get_password_for_username(name)
-        if password == '':
+        password = keyring.get_password(app_name, username)
+        if password is None:
             password = getpass.getpass()
         else:
             password_loaded = True
 
-        try:
-            mail = imaplib.IMAP4_SSL('imap.gmail.com')
-            mail.login(name, password)
-            if not password_loaded:
-                self.save_password_for_username(name, password)
+        mail = imaplib.IMAP4_SSL('imap.gmail.com')
+        mail.login(username, password)
+        if not password_loaded:
+            keyring.set_password(app_name, username, password)
 
-            mail.select()
-            result, data = mail.uid('search', None, 'ALL')
-            elements = data[0].split()
-            header_parser = BytesHeaderParser()
-            # for element in elements:
-            # result, data = mail.element('fetch', element, '(BODY[HEADER])')
-            # msg = header_parser.parsestr(data[0][1])
-            # print(msg['From'], msg['Date'], msg['Subject'])
+        mail.select()
+        result, data = mail.uid('search', None, 'ALL')
+        elements = data[0].split()
+        header_parser = BytesHeaderParser()
+        # for element in elements:
+        # result, data = mail.element('fetch', element, '(BODY[HEADER])')
+        # msg = header_parser.parsestr(data[0][1])
+        # print(msg['From'], msg['Date'], msg['Subject'])
 
-            for element in elements:
-                result, data = mail.uid('fetch', element, '(RFC822)')
-                raw_mail = data[0][1]
-                header = header_parser.parsebytes(raw_mail)
-                message = email.message_from_bytes(raw_mail)
-                # print('Message: ' + self.getMailText(message).strip() + "\n")
+        for element in elements:
+            result, data = mail.uid('fetch', element, '(RFC822)')
+            raw_mail = data[0][1]
+            header = header_parser.parsebytes(raw_mail)
+            message = email.message_from_bytes(raw_mail)
+            # print('Message: ' + self.getMailText(message).strip() + "\n")
 
-                if '#mb' in header['Subject']:
-                    print('From: ' + header['From'])
-                    print('Date: ' + header['Date'])
-                    print('Subject: ' + header['Subject'])
-                    text = self.get_mail_text(message).strip()
-                    self.parse_text(text)
-                    mail.uid('store', element, '+FLAGS', '\\Deleted')
+            if '#mb' in header['Subject']:
+                print('From: ' + header['From'])
+                print('Date: ' + header['Date'])
+                print('Subject: ' + header['Subject'])
+                text = self.get_mail_text(message).strip()
+                self.parse_text(text)
+                mail.uid('store', element, '+FLAGS', '\\Deleted')
 
-            mail.expunge()
-            mail.close()
-            mail.logout()
-
-        except Exception as e:
-            print('Error: ' + format(e))
+        mail.expunge()
+        mail.close()
+        mail.logout()
 
         return 0
+
+    @staticmethod
+    def get_valid_commands():
+        return ['help', 'eat']
--- tests/test_mail_bug.py
+++ tests/test_mail_bug.py
@@ -2,5 +2,5 @@ from mail_bug.mail_bug import MailBug
 
 
 def test_return_code():
-    mb = MailBug()
-    assert mb.run() == 0
+    bug = MailBug([])
+    assert bug.run() == 0
