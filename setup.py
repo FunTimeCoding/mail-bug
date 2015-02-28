4a4eb59fd30361fd7a8f0af3b060af0ac0ed4df4
Alexander Reitzel <funtimecoding@gmail.com>
2015-02-28 19:32:09 +0100

    add python_utility dependency to setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,19 @@ try:
 except ImportError:
     from distutils.core import setup
 
-setup(name='mail-bug',
-      version='0.1',
-      description='Stub description for mail-bug.',
-      install_requires=['keyring'],
-      scripts=['bin/mb'],
-      packages=[],
-      author='Alexander Reitzel',
-      author_email='funtimecoding@gmail.com',
-      url='http://example.org',
-      download_url='http://example.org/mail-bug.tar.gz')
+setup(
+    name='mail-bug',
+    version='0.1',
+    description='Stub description for mail-bug.',
+    install_requires=['keyring', 'python_utility==0.1'],
+    scripts=['bin/mb'],
+    packages=[],
+    author='Alexander Reitzel',
+    author_email='funtimecoding@gmail.com',
+    url='http://example.org',
+    download_url='http://example.org/mail-bug.tar.gz',
+    dependency_links=[
+        'git+git://github.com/FunTimeCoding/python-utility.git'
+        '#egg=python_utility-0.1'
+    ]
+)
